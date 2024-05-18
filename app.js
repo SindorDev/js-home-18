ea20348bfe04143177bb247561ca3ab9d784c2bb
Sindor007 <thesso003@gmail.com>
2024-05-18 22:13:19 +0500

    finished

--- app.js
+++ app.js
@@ -1,93 +1,72 @@
 const addBtn = document.querySelector("#add-btn");
 const showBtn = document.querySelector("#show-btn");
+const result = document.querySelector("#result");
 const deleteBtn = document.querySelector("#delete-btn");
 const sortBtn = document.querySelector("#sort-btn");
-const result = document.querySelector("#result");
-
-function NewTodo(ntn, h, m, s, t){
-    this.id = (Math.floor(Math.random() * 100000)+ 1).toString().padStart(6, "0")
-    this.name = ntn
-    this.hours = h.toString().padStart(2, "0")
-    this.minutes = m.toString().padStart(2, "0")
-    this.seconds = s.toString().padStart(2, "0")
-    this.time = t
+function newTodo(info, time) {
+    this.id = (Math.floor(Math.random() * 100000)+ 1).toString().padStart(6, "0");
+    this.info = info,
+    this.time = time
 }
 
-const ALL_TODOS = JSON.parse(localStorage.getItem("todos")) || [];
+const local = JSON.parse(localStorage.getItem("Info")) || []
+const AddTodo = () => {
+    let info = prompt("Enter your Name");
+    let time = new Date().toLocaleTimeString();
 
-const addNewTodo = function(){
-    let newTodoName = prompt("Enter new todoname: ");
-    if(newTodoName && newTodoName.trim() !== ""){
-            let date = new Date()
-            let hours = date.getHours();
-            let minutes = date.getMinutes();
-            let seconds = date.getSeconds();
-            let time = date.getTime();
-            
-            // TODO OBJECT CREATE
-            let newTodoResult = new NewTodo(newTodoName, hours, minutes, seconds, time);
+    let newObject = new newTodo(info, time)
+    local.push(newObject)
 
-            ALL_TODOS.push(newTodoResult);
-            localStorage.setItem("todos", JSON.stringify(ALL_TODOS));
-    }
-    else{
-            alert("Please enter todo name!")
-    }
+    localStorage.setItem("Info", JSON.stringify(local))
 }
-
-
-const showTodos = function(){
+const showTodo = () => {
     result.innerHTML = ""
-    ALL_TODOS.forEach(todo => {
+    local.forEach(item => {
         result.innerHTML += `
-            <div class="todo-item">
-                <span>${todo.id}</span>
-                <h2>${todo.name}</h2>
-                <span>${todo.hours} : ${todo.minutes} : ${todo.seconds}</span>
-            </div>
+        <div class="group">
+            <span>${item.id}</span>
+            <h2>${item.info}</h2>
+            <span>${item.time}</span>
+        </div>
         `
     })
 }
+const deleteId = () => {
+    let delId = prompt("Enter Id: ");
 
-
-const deleteTodo = function(){
-    let deleteId = prompt("Enter todo id: ");
-
-    if(deleteId){
-        let indexOfDeletedElement = ALL_TODOS.findIndex(element => element.id === deleteId);
-        ALL_TODOS.splice(indexOfDeletedElement, 1);
-        localStorage.setItem("todos", JSON.stringify(ALL_TODOS));
+    if(delId) {
+        let findIndex = local.findIndex(element => element.id === delId);
+        local.splice(findIndex, 1);
+        localStorage.setItem("Info", JSON.stringify(local))
     }
 }
-let sorted = false;
-const sortTodo = function(){
-
-    if(sorted === false){
-        sorted = true;
-        ALL_TODOS.sort((a, b) => {
-            if(a.time > b.time){
+let sort = false
+const sortedShow = () => {
+    if(sort === false) {
+        sort === true;
+        local.sort((a,b) => {
+            if(a.time > b.time) {
                 return -1
             }
-            else{
+            else {
                 return 1
             }
         })
     }
-    else{
-        sorted = false;
-        ALL_TODOS.sort((a, b) => {
-            if(a.time > b.time){
-                return 1
-            }
-            else{
-                return -1
-            }
-        })
+    else {
+        if(a.time > b.time) {
+            return 1
+        }
+        else {
+            return -1
+        }
     }
-    
-    showTodos();
+
+    showTodo()
 }
-addBtn.addEventListener("click", addNewTodo);
-showBtn.addEventListener("click", showTodos);
-deleteBtn.addEventListener("click", deleteTodo);
-sortBtn.addEventListener("click", sortTodo);
+
+
+addBtn.addEventListener("click", AddTodo)
+showBtn.addEventListener("click", showTodo);
+deleteBtn.addEventListener("click", deleteId);
+sortBtn.addEventListener("click", sortedShow);
